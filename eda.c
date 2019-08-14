ff174e0ffeb6282c2b317c6ed362dbec5d120efb
Victor <eyervictor@gmail.com>
2019-08-14 02:38:05 -0300

    any click size

--- eda.c
+++ eda.c
@@ -50,38 +50,26 @@ void main(){
 	srand(time(0)); 
 	int num,k,l,achou,conta,pos;
 	
-
-	/*
-	###### Pega vértices aleatórios ###### 
-	######  Em conta < 3 é selecionado  ###### 
-	######   o tamanho da clique   ###### 
-	*/
-/*
-	while(conta < 3) {
-		int num = (rand() % m);
-		int achou = 0;
-
-		for (i = 0; i < 3; i++) {
-			if(vet[i] ==num) {
-				achou = 1;
-				break;
-			}
+	do{
+		printf("\n\nDigite o tamanho K da clique que deseja encontrar: ");
+		scanf("%d", &k);
+	
+		if(k>m){
+			printf("Tamanho de clique maior que a clique maximal possivel.");
+		}else if(k==m){
+			printf("Tamanho da clique nao pode ser igual ao numero de vertices (deve ser um subgrafo).");
+		}else if(k<1){
+			printf("Tamanho da clique nao pode ser menor que 0");
 		}
+	}while(k>=m || k<1);	
+	int vet[k];
+	
+	// ###### gera vertices aleatorios #######
+	
+	
 
-		if(!achou) {
-			vet[conta] = num;
-			conta++;
-		}
-	}
-	for (k = 0; k < 3; k++){
-		printf("%d ", vet[k]);
-	}
-
-
-*/
-
-system("cls");
-	int codigo, vet[50], v;
+	system("cls");
+	int codigo, v;
 	do{
 		achou=0;
 		conta=0;
@@ -95,62 +83,44 @@ system("cls");
 		scanf("%d", &codigo);
 		switch (codigo){
 			case 1 : 
-				while(conta < 3) {
-					int num = (rand() % m);
+				while(conta < k) {
+					int num = (rand() % m - 1) + 1;
 					int achou = 0;
-					for (i = 0; i < 3; i++) {
+					for (i = 0; i < k; i++) {
 						if(vet[i] ==num) {
 							achou = 1;
 							break;
 						}
 					}
-
 					if(!achou) {
 						vet[conta] = num;
 						conta++;
 					}
 				}
 				printf("\nVertices escolhidos: ");
-				for (k = 0; k < 3; k++){
-					printf("%d ", vet[k]);
+				for (i = 0; i < k; i++){
+					printf("%d ", vet[i]);
 				}
-				for (i = 0; i < 3; i++){
-					for (j = 0; j < 3; j++){
-						if(matriz[vet[i]][vet[j]] == 1 && vet[i]!=vet[j]){
-							v=1;
-						}	
-                        else if (matriz[vet[i]][vet[j]] == 0 && vet[i]!=vet[j]){
-                            v=0;
-                            break;
-                        }
-                    }
-                }
-                if(v==1){
-					printf("\nEsses vertices formam uma clique!\n\n");
-				} else {
-					printf("\nEsses vertices nao formam uma clique\n\n");
-				}
-				
-					// if (matriz[vet[0]][vet[1]] == 1 && matriz[vet[0]][vet[2]] == 1 && matriz[vet[1]][vet[2]] == 1){
-					// 	printf("\nEsses vertices formam uma clique!\n\n");
-					// } else{
-					// 	printf("\nEsses vertices nao formam uma clique\n\n");
-					// }
-				
-			break;
+				if(k==1){
+					printf("\nEsse vertice forma uma clique!");
+				}else if(k==2){
+					if(matriz[vet[0]][vet[1]]==1)printf("\nEsses vertices formam uma clique!");
+					else printf("\nEsses vertices nao formam uma clique!");
+				}else{
+					for(i=0;i<k;i++){
+						for(j=0;j<k;j++){
+							if(matriz[vet[i]][vet[j]] == 1 && vet[i]!=vet[j])v=1;
+							else if (matriz[vet[i]][vet[j]] == 0 && vet[i]!=vet[j]){
+								v=0;
+								break;
+							}
+						}
+					}
+					if(v==1)printf("\nEsses vertices formam uma clique!\n\n");
+					else printf("\nEsses vertices nao formam uma clique\n\n");
+				}						
 		}		
 	} while (codigo != 6);
 	
-	
-	// for (i=0;i<3;i++){
-	// 	pos = vet[i];
-	// 	for(j=0;j<3;j++){
-	// 		if (pos == j){ } else {
-	// 			if (matriz[pos][j] == 0){
-					
-	// 			}
-	// 		}
-	// 	}
-	// }
 	
 }
